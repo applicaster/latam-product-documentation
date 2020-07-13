000ad65499bd379337049f00ab10aa19493fa4af
arturoapplicaster <37178009+arturoapplicaster@users.noreply.github.com>
2020-07-13 13:38:26 -0500

    Create Inplayer_jw_arturo.js

---
+++ Test_feeds/Inplayer_jw_arturo.js
@@ -0,0 +1,38 @@
+{
+  "type": {
+    "value": "feed"
+  },
+  "entry": [
+    {
+      "type": {
+        "value": "video"
+      },
+      "id": "xF7Hk51k",
+      "title": "Hotel Transylvania 3",
+      "summary": "On a luxury monster cruise ship Dracula meets Ericka, the captain of the ship, who belongs to the family of Van Helsing that wants to destroy the entire monster race.",
+      "published": "2019-07-31T00:00:00+00:00",
+      "media_group": [
+        {
+          "type": "image",
+          "media_item": [
+            {
+              "src": "https://raw.githubusercontent.com/applicaster/latam-product-documentation/master/Test_feeds/hotel%20transylvania%203.jpg",
+              "key": "image_base"
+            }
+          ]
+        }
+      ],
+      "extensions": {
+        "event_id": 3741,
+        "event_inplayer_id": "73800",
+        "isLive": false,
+        "requires_authentication": true,
+        "ds_product_ids": [
+          "73800"
+        ],
+        "startTimeEast": "Tuesday, July 14, 5:00PM EDT"
+      }
+    }
+  ],
+  "title": "Subscription"
+}
