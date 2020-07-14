aaf1a904f648680c3f920f12ea3cb317edb40f43
arturoapplicaster <37178009+arturoapplicaster@users.noreply.github.com>
2020-07-13 20:28:26 -0500

    Create Inplayer_arturo_loginonly.js

---
+++ Test_feeds/Inplayer_arturo_loginonly.js
@@ -0,0 +1,38 @@
+{
+  "type": {
+    "value": "feed"
+  },
+  "entry": [
+    {
+      "type": {
+        "value": "video"
+      },
+      "id": "IkKwwZcK",
+      "title": "Tomb Raider",
+      "summary": "Lara Croft, a courageous and independent young woman, sets out on a dangerous journey to unravel the truth behind her adventurer father's mysterious disappearance",
+      "published": "2019-07-31T00:00:00+00:00",
+      "media_group": [
+        {
+          "type": "image",
+          "media_item": [
+            {
+              "src": "https://raw.githubusercontent.com/applicaster/latam-product-documentation/master/Test_feeds/hotel%20transylvania%203.jpg",
+              "key": "image_base"
+            }
+          ]
+        }
+      ],
+      "extensions": {
+        "event_id": 3742,
+        "event_inplayer_id": "73821",
+        "isLive": false,
+        "requires_authentication": true,
+        "ds_product_ids": [
+          "73821"
+        ],
+        "startTimeEast": "Tuesday, July 14, 5:00PM EDT"
+      }
+    }
+  ],
+  "title": "Login"
+}
