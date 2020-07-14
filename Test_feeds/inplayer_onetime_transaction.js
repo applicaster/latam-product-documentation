c661c4573dee2202205b053bf8a7089b79cbcafa
arturoapplicaster <37178009+arturoapplicaster@users.noreply.github.com>
2020-07-13 21:11:43 -0500

    Create inplayer_onetime_transaction.js

---
+++ Test_feeds/inplayer_onetime_transaction.js
@@ -0,0 +1,68 @@
+{
+  "type": {
+    "value": "feed"
+  },
+  "entry": [
+    {
+      "type": {
+        "value": "video"
+      },
+      "id": "1CUJ2aM1",
+      "title": "Red Sparrow",
+      "summary": "Dominika Egorova, a former ballerina, enrolls in a Russian Intelligence program and becomes a sparrow, using her body as a weapon to entrap a CIA agent",
+      "published": "2019-07-31T00:00:00+00:00",
+      "media_group": [
+        {
+          "type": "image",
+          "media_item": [
+            {
+              "src": "https://raw.githubusercontent.com/applicaster/latam-product-documentation/master/Test_feeds/1*_Yww999PonWWXVKajowP4g.jpeg",
+              "key": "image_base"
+            }
+          ]
+        }
+      ],
+      "extensions": {
+        "event_id": 3743,
+        "event_inplayer_id": "73822",
+        "isLive": false,
+        "requires_authentication": true,
+        "ds_product_ids": [
+          "73822"
+        ],
+        "startTimeEast": "Tuesday, July 14, 5:00PM EDT"
+      }
+    },
+    {
+      "type": {
+        "value": "video"
+      },
+      "id": "OhWpdw3n",
+      "title": "Jurassic World: Fallen Kingdom",
+      "summary": "After a volcano eruption proves to be a threat for the dinosaurs, Owen and Claire reach the defunct Jurassic World, a theme park, to save the animals from extinction",
+      "published": "2019-07-31T00:00:00+00:00",
+      "media_group": [
+        {
+          "type": "image",
+          "media_item": [
+            {
+              "src": "https://raw.githubusercontent.com/applicaster/latam-product-documentation/master/Test_feeds/jurassic-world-fallen-kingdom-review-1200.jpg",
+              "key": "image_base"
+            }
+          ]
+        }
+      ],
+      "extensions": {
+        "event_id": 3744,
+        "event_inplayer_id": "73823",
+        "isLive": false,
+        "requires_authentication": true,
+        "ds_product_ids": [
+          "73823"
+        ],
+        "startTimeEast": "Tuesday, July 14, 5:00PM EDT"
+      }
+    }
+  ],
+  "title": "One-Time Transaction"
+}
